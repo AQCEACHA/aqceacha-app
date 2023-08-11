a82eac55d7420de5d7f4164fdb239326add178f4
Victor Carbelotti <victorcarbelotti0306@gmail.com>
2023-08-11 03:47:46 -0300

    Carousel e Preguiça

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, SafeAreaView, StatusBar, View, TextInput } from "react-native";
+import { StyleSheet, SafeAreaView, StatusBar, View, TextInput, Text } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import Home from "./src/pages/Home";
-import { wrap } from "module";
+import { Carousel } from "./src/pages/Carousel";
+import { BounceOutLeft } from "react-native-reanimated";
 
 export default function App() {
   return (
@@ -9,9 +10,24 @@ export default function App() {
       <StatusBar />
       <Home />
       <View style={styles.search}>
-      <Feather name="search" size={25} color="#10228A" style={styles.iconsearch}/>
-      <TextInput></TextInput>
+        <Feather name="search" size={25} color="#10228A" style={styles.iconsearch}/>
+      <TextInput placeholder="O que você está procurando?" style={styles.textinput}>
+      </TextInput>
       </View>
+
+      <View style={styles.carousel}>
+        <Carousel />
+      </View>
+
+      <View style={styles.cat}>
+        <Text>Categorias</Text>
+        <View style={styles.icons}>
+        <Feather name="camera" size={25} color="#10228A" /> <Text>Fotógrafo</Text>
+        <Feather name="cpu" size={25} color="#10228A" /> <Text>T.I</Text>
+        <Feather name="tool" size={25} color="#10228A"/> <Text>Mecânico</Text>
+        </View>
+      </View>
+
     </SafeAreaView>
   );
 }
@@ -21,17 +37,34 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#eeeeee",
   },
+  carousel: {
+    alignItems: 'center',
+    paddingTop: 5,
+  },
   search: {
-    alignContent: 'center',
+    flexDirection: 'row',
     marginTop: 24,
-    marginBottom: 24,
+    marginBottom: 20,
     backgroundColor: 'white',
     alignSelf: 'center',
-    height: 24,
+    height: 40,
     width: 312,
     borderRadius: 25,
   },
   iconsearch: {
+    marginLeft: 12,
+    marginRight: 15,
+    alignSelf: 'center'
+  },
+  textinput: {
+    alignSelf: 'center'
+  },
+  cat: {
+    
+    
+  },
+  icons: {
+   flexDirection: 'row',
 
-  }
+  },
 });
